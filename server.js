f54eaa3d44f66c03a19cddb6fdf48d52430be112
Glitch (rocky-tame-cabin) <none>
2020-07-11 14:46:34 +0000

    💄🌱 Checkpoint ./server.js:16377575/417

--- server.js
+++ server.js
@@ -4,10 +4,12 @@
 // init project
 var express = require("express");
 var app = express();
-
+var bodyparser = require('body-parser');
+app.use(bodyparser.json());
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
+app.use(cors());
 app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,7 +26,12 @@ app.get("/api/timestamp/:date_string?", function(req, res) {
   //res.json({ greeting: "hello API" });
   //res.json({ greeting: "hello API" });
   let dateval = req.params.date_string;
-  console.log(datestring);
+  console.log(dateval);
+  var dateFormattingOption = {
+    year : 'numeric',
+    month : 'long',
+    day : 'numeric'
+  }
   /*if(datestring == "")
     {
       res.json({ greeting: "nothing given" });
@@ -36,16 +43,17 @@ app.get("/api/timestamp/:date_string?", function(req, res) {
   if(isNaN(dateval))
     {
       var naturalDate = new Date(dateval);
-      naturalDate = naturalDate.toLocalString();
-      var unixDate = new Date(dateVal).getTime()/1000;
+      naturalDate = naturalDate.toLocalDateString("en-us",dateFormattingOption);
+      var unixDate = new Date(dateval).getTime()/1000;
       
     }
   else
     {
       var unixDate = dateval;
       var naturalDate = new Date(dateval * 1000);
-      naturalDate = naturalDate
+      naturalDate = naturalDate.toLocalDateString("en-us",dateFormattingOption);
     }
+  res.json({ unix : unixDate, natural : naturalDate });
 });
 
 // listen for requests :)
